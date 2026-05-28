33e0c27a8371649a731656a61ada94a8d14f3265
JaiJai <wm.rd01@gmail.com>
2026-05-28 11:39:48 +0800

    拿掉debug模式，完善斷線機制

--- assets/BackGround/index.js
+++ assets/BackGround/index.js
@@ -1,8 +1,4 @@
-System.register("chunks:///_virtual/BackGround", [], function () {
-  return {
-    execute: function () {}
-  };
-});
+System.register("chunks:///_virtual/BackGround",[],(function(){return{execute:function(){}}}));
 
 (function(r) {
   r('virtual:///prerequisite-imports/BackGround', 'chunks:///_virtual/BackGround'); 
--- assets/Table/index.js
+++ assets/Table/index.js
@@ -1,8 +1,4 @@
-System.register("chunks:///_virtual/Table", [], function () {
-  return {
-    execute: function () {}
-  };
-});
+System.register("chunks:///_virtual/Table",[],(function(){return{execute:function(){}}}));
 
 (function(r) {
   r('virtual:///prerequisite-imports/Table', 'chunks:///_virtual/Table'); 
